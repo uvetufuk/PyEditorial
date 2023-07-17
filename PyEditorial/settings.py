b21d4885450dd7745a924cfdc5bb6e6be703ea75
uvetufuk <uvetufuk@gmail.com>
2023-07-17 14:44:26 +0300

    Fix Db Name

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'PyEditorial',
+         'NAME': 'pyeditorialdb',
          'USER': 'postgres',
          'PASSWORD': 'great123',
          'HOST': 'postgres',
